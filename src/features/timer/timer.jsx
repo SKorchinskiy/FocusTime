a7acbe5f91f9ddeda60ac376af443749fb2c24f3
SKorchinskiy <ddphyk@gmail.com>
2024-02-04 02:42:48 +0200

    + update timer component

--- src/features/timer/timer.jsx
+++ src/features/timer/timer.jsx
@@ -1,7 +1,7 @@
-import { useEffect, useRef, useState } from "react";
-import { View, Text, StyleSheet } from "react-native";
-import colors from "../../utils/colors";
-import sizes from "../../utils/sizes";
+import {useEffect, useRef, useState} from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+import colors from '../../utils/colors';
+import sizes from '../../utils/sizes';
 
 export const TIME_CONSTANTS = {
   millisecond: 1,
@@ -14,22 +14,22 @@ function formatTime(timeInMillis) {
   const seconds =
     (timeInMillis % TIME_CONSTANTS.minute) / TIME_CONSTANTS.second;
 
-  return `${minutes >= 10 ? "" : "0"}${minutes}:${
-    seconds >= 10 ? "" : "0"
+  return `${minutes >= 10 ? '' : '0'}${minutes}:${
+    seconds >= 10 ? '' : '0'
   }${seconds}`;
 }
 
-function parseInitialTime(initialTime) {
-  if (typeof initialTime === "undefined") return 10 * TIME_CONSTANTS.second;
-  if (typeof initialTime === "number") return initialTime;
+function parseTime(currentTime) {
+  if (typeof currentTime === 'undefined') return 10 * TIME_CONSTANTS.second;
+  if (typeof currentTime === 'number') return currentTime;
   try {
     const [timeValue, timeType] = [
-      initialTime.slice(0, initialTime.length - 1),
-      initialTime[initialTime.length - 1],
+      currentTime.slice(0, currentTime.length - 1),
+      currentTime[currentTime.length - 1],
     ];
 
     const timeInSec =
-      timeType === "s"
+      timeType === 's'
         ? parseInt(timeValue) * TIME_CONSTANTS.second
         : parseInt(timeValue) * TIME_CONSTANTS.minute;
 
@@ -40,35 +40,33 @@ function parseInitialTime(initialTime) {
 }
 
 export default function Timer({
-  initialTime = "6s",
+  currentTime = '60s',
   isStarted = false,
-  updateInitialTime = () => {},
+  updateCurrentTime = () => {},
 }) {
-  const [expirationTime, setExpirationTime] = useState(
-    parseInitialTime(initialTime)
-  );
+  const [expirationTime, setExpirationTime] = useState(parseTime(currentTime));
   const intervalId = useRef(null);
 
   useEffect(() => {
-    setExpirationTime(parseInitialTime(initialTime));
+    setExpirationTime(parseTime(currentTime));
 
     if (expirationTime > 0) {
       if (intervalId.current) clearInterval(intervalId.current);
 
       if (isStarted) {
         const timerId = setInterval(
-          () => setExpirationTime((value) => value - TIME_CONSTANTS.second),
-          TIME_CONSTANTS.second
+          () => setExpirationTime(value => value - TIME_CONSTANTS.second),
+          TIME_CONSTANTS.second,
         );
         intervalId.current = timerId;
       }
     }
-  }, [initialTime, isStarted]);
+  }, [currentTime, isStarted]);
 
   useEffect(() => {
     if (expirationTime === 0) clearInterval(intervalId.current);
-    if (parseInitialTime(initialTime) !== expirationTime)
-      updateInitialTime(expirationTime);
+    if (parseTime(currentTime) !== expirationTime)
+      updateCurrentTime(expirationTime);
   }, [expirationTime]);
 
   return (
@@ -81,15 +79,15 @@ export default function Timer({
 const styles = StyleSheet.create({
   timerContainer: {
     backgroundColor: colors.purple,
-    borderRadius: "10",
-    justifyContent: "center",
-    alignItems: "center",
+    borderRadius: '10',
+    justifyContent: 'center',
+    alignItems: 'center',
     width: 300,
     height: 150,
   },
   timerTime: {
     fontSize: sizes.xl,
     color: colors.white,
-    fontWeight: "bold",
+    fontWeight: 'bold',
   },
 });
